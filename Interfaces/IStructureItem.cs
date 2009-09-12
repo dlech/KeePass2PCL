a58ee3ac8a9cb5f296fe4f4c3f40ec0f8aa49e9a
Dominik Reichl <dominik.reichl@t-online.de>
2009-09-12 10:32:00 -0500

    KeePass 2.09

--- Interfaces/IStructureItem.cs
+++ Interfaces/IStructureItem.cs
@@ -17,26 +17,21 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System;
 
-// General assembly properties
-[assembly: AssemblyTitle("KeePassLib")]
-[assembly: AssemblyDescription("KeePass Password Management Library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dominik Reichl")]
-[assembly: AssemblyProduct("KeePassLib")]
-[assembly: AssemblyCopyright("Copyright © 2003-2009 Dominik Reichl")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+namespace KeePassLib.Interfaces
+{
+	public interface IStructureItem : ITimeLogger // Provides LocationChanged
+	{
+		PwUuid Uuid
+		{
+			get;
+			set;
+		}
 
-// COM settings
-[assembly: ComVisible(false)]
-
-// Assembly GUID
-[assembly: Guid("395f6eec-a1e0-4438-aa82-b75099348134")]
-
-// Assembly version information
-[assembly: AssemblyVersion("2.0.9.*")]
-[assembly: AssemblyFileVersion("2.0.9.0")]
+		PwGroup ParentGroup
+		{
+			get;
+		}
+	}
+}
